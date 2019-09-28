71bd2e7e4a39a8f6487b82f14021e7cb6eefa220
ajay1706 <ajay97456@gmail.com>
2019-09-28 20:50:40 +0530

    adding card'

--- flight_app/lib/flight_list.dart
+++ flight_app/lib/flight_list.dart
@@ -44,6 +44,36 @@ class FlightListTopPart extends StatelessWidget {
                 gradient: LinearGradient(colors: [firstColor, secondColor])),
             height:160 ,
           ),
+        ),
+        Column(
+          children: <Widget>[
+            SizedBox(height: 20,),
+            Card(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+              margin: EdgeInsets.symmetric(horizontal: 16),
+              elevation: 10,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal:16, vertical: 20),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text("Boston [BOS]", style:TextStyle(fontSize:16.0, )),
+                        Divider(color: Colors.grey,),
+                        Text("New York City [JFK]", style:TextStyle(fontSize:16.0,fontWeight: FontWeight.bold )),
+                      ],
+                    ),
+                    Icon(Icons.import_export,color:Colors.black)
+                  ],
+
+                ),
+              ),
+            ),
+          ],
         )
       ],
 
